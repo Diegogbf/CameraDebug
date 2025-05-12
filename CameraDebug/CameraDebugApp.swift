40dccaf14631377f1d175fd645dd3a5b9bc471b6
Diego Fernandes <diego.gbfernandes@gmail.com>
2025-05-11 22:08:02 -0300

    Initial Commit

---
+++ CameraDebug/CameraDebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraDebugApp.swift
+//  CameraDebug
+//
+//  Created by Diego Gomes Basilio Fernandes on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CameraDebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CameraDebugTests/CameraDebugTests.swift
@@ -0,0 +1,17 @@
+//
+//  CameraDebugTests.swift
+//  CameraDebugTests
+//
+//  Created by Diego Gomes Basilio Fernandes on 5/11/25.
+//
+
+import Testing
+@testable import CameraDebug
+
+struct CameraDebugTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
